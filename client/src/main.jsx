c6d23575b73bfc9b139b59959169a2d695df77b9
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-04 21:07:11 +0100

    Project setup #1, client side entrance point edited for react app with routing.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "./Routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
