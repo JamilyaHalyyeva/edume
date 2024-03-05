852bec265a805e07c807a03f1323466ac99178b5
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-05 21:01:03 +0100

    adds: User model is added

---
+++ server/src/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ['teacher', 'student', 'admin'],
+    default: 'student',
+    required: true,
+    trim: true,
+  },
+})
+const User = mongoose.model('User', userSchema, 'users')
+export default User
