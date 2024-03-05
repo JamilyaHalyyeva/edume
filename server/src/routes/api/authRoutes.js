d9ade5a7dea1628408353652a1a8492e67a2c646
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-05 21:00:01 +0100

    adds: authroutes register endpoint

--- server/src/routes/api/authRoutes.js
+++ server/src/routes/api/authRoutes.js
@@ -1,3 +1,7 @@
 import express from 'express'
+import { handleRegister } from '../../controllers/authController.js'
 
 const authRouter = express.Router()
+authRouter.post('/register', handleRegister)
+
+export default authRouter
