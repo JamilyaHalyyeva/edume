fc442f4a618feb6b9823c326b96609e4ec10cbe3
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-06 16:14:19 +0100

    adds: changeforgotpassword route is added

--- server/src/routes/api/authRoutes.js
+++ server/src/routes/api/authRoutes.js
@@ -3,11 +3,13 @@ import {
   handleRegister,
   handleLogin,
   handleForgotPassword,
+  handleChangeForgotPassword,
 } from '../../controllers/authController.js';
 
 const authRouter = express.Router();
 authRouter.post('/register', handleRegister);
 authRouter.post('/login', handleLogin);
 authRouter.post('/forgotpassword', handleForgotPassword);
+authRouter.patch('/changeforgotpassword', handleChangeForgotPassword);
 
 export default authRouter;
