f703e3f7e9c6c604aa55bd8283348559e13dcca4
JamilyaHalyyeva <33869456+JamilyaHalyyeva@users.noreply.github.com>
2024-03-07 19:19:08 +0100

    edits : dialogProvider was overkill and just deleted it  and adjust the way we open role selection dialog. (#15)

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import { DialogProvider } from "./context/DialogProvider";
 
 const AppRoutes = () => {
   return (
-    <DialogProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </DialogProvider>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 };
 
