67cc74dc8643708a5435d62380e5744df0ece877
Jamilya Halyyeva <jamilya.halyyeva@gmail.com>
2024-03-07 22:44:08 +0100

    adds: template for dashboard selection , need Usercontext instead of localstorage

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import DashboardPage from "./pages/DashboardPage";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   );
 };
