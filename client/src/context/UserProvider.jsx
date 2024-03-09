bd209790440c71605e30125dd209d92e60fcfd01
Jamilya Halyyeva <jamilya.halyyeva@gmail.com>
2024-03-09 20:45:09 +0100

    adds: user context

--- client/src/context/UserProvider.jsx
+++ client/src/context/UserProvider.jsx
@@ -1,5 +1,58 @@
-function UserProvider() {
-  return <div>UserProvider</div>;
-}
+// userContext.js
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
 
-export default UserProvider;
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginUser = async (userData) => {
+    try {
+      const response = await axios.post("/api/auth/login", userData);
+      const { token } = response.data;
+      localStorage.setItem("token", token); // Save token in localStorage for persistence
+      setUser(userData);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Error in loginUser:", error.message);
+      // Handle login error (show message, redirect, etc.)
+    }
+  };
+
+  const registerUser = async (userData) => {
+    try {
+      await axios.post("/api/auth/register", userData);
+      // Optionally, you can automatically log in the user after registration
+      loginUser(userData);
+    } catch (error) {
+      console.error("Error in registerUser:", error.message);
+      // Handle registration error (show message, redirect, etc.)
+    }
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("token"); // Remove token from localStorage
+    setUser(null);
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ user, isAuthenticated, loginUser, registerUser, logoutUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
+
+export { UserProvider, useUser };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes.jsx";
+import { UserProvider } from "./context/UserProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes />
+      <UserProvider>
+        <Routes />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
