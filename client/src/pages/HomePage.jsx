cbaf7c2b3268760d713e0c15820903c0369a1c34
Jamilya Halyyeva <jamilya.halyyeva@gmail.com>
2024-03-10 18:58:33 +0100

    adds: simple login button to redirect to login page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import UserRolesDialog from "../components/UserRolesDialog";
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 function HomePage() {
+  const navigate = useNavigate();
   const [openDialog, setOpenDialog] = useState(false);
   const handleButtonClick = () => {
     setOpenDialog(true);
+  };
+  const handleLoginClick = () => {
+    navigate("/login");
   };
   return (
     <div className="bg-gray-100 h-screen flex items-center justify-center">
@@ -20,6 +24,12 @@ function HomePage() {
           className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
         >
           Open Dialog
+        </button>
+        <button
+          onClick={handleLoginClick}
+          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Login
         </button>
       </div>
       <UserRolesDialog isDialogOpen={openDialog} />
