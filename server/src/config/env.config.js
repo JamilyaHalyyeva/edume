6eeccf82c98bc7084b237fc75bd43719e697ff10
omolara olugbile <o.olugbile@yahoo.com>
2024-03-11 10:04:52 +0100

    coursecard

--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -3,6 +3,7 @@ dotenv.config()
 const config = {
   port: process.env.PORT || 3000,
   dbUri: process.env.DB_URI,
+  
   // Add other environment variables here
 }
 export default config
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import aboutCourse from '../courseCard.js'
 import config from './config/env.config.js'
 import loggerMiddleware from './middlewares/loggerMiddleware.js'
 import defaultRouter from './routes/api/defaultRoute.js'
@@ -7,13 +8,19 @@ import cors from 'cors'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json(aboutCourse)
+
+})
 
 connectToDatabase()
 app.use('/api', defaultRouter)
 app.use(loggerMiddleware)
 
 const PORT = config.port || 3000
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
