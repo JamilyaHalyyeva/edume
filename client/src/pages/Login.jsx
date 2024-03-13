968ae4e32b1ecfbf5edf770117d57a02fad6cc42
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-14 00:47:10 +0100

    fixed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -383,37 +383,6 @@ const LoginPage = () => {
         </form>
       </div>
     </div>
-
-    // <div className="mx-auto max-w-sm p-6 bg-white rounded-md shadow-md">
-    //   <h2 className="text-2xl font-semibold mb-4">Login</h2>
-    //   <div className="mb-4">
-    //     <label>Email:</label>
-    //     <input
-    //       className="border border-gray-300 p-2 w-full rounded-md"
-    //       type="text"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //     />
-    //     {emailError && <div className="text-red-500">{emailError}</div>}
-    //   </div>
-    //   <div className="mb-4">
-    //     <label>Password:</label>
-    //     <input
-    //       className="border border-gray-300 p-2 w-full rounded-md"
-    //       type="password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     {passwordError && <div className="text-red-500">{passwordError}</div>}
-    //   </div>
-    //   {error && <div className="text-red-500 mb-4">{error}</div>}
-    //   <button
-    //     className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-    //     onClick={handleLogin}
-    //   >
-    //     Login
-    //   </button>
-    // </div>
   );
 };
 
