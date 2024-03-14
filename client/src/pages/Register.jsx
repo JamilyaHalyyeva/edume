7436493962d741300a1089ea632c49b72d64e1b7
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-03-14 19:08:09 +0100

    edits: register page's responsivity and styling are done.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,20 @@
 // RegisterPage.js
 import { useState } from "react";
 import { useUser } from "../context/UserProvider";
-import StudentPreProfilePage from "./StudentPreProfilePage";
+import LOGO from "../assets/logo.png";
+import REGISPANDA from "../assets/panda.png";
+// import GOOGLEICON from "../assets/googleicon.png";
+import EDUME from "../assets/edume.png";
+import { Link } from "react-router-dom";
+// import BIO from "../assets/bio.png";
+// import { motion } from "framer-motion";
+// import CHIM from "../assets/chim.png";
+// import COMP from "../assets/comp.png";
+// import MATH from "../assets/math.png";
+// import CUL from "../assets/cul.png";
+// import MUS from "../assets/mus.png";
+// import ENG from "../assets/eng.png";
+// import StudentPreProfilePage from "./StudentPreProfilePage";
 
 const RegisterPage = () => {
   const { registerUser } = useUser();
@@ -16,7 +29,7 @@ const RegisterPage = () => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const role = localStorage.getItem("role");
+  // const role = localStorage.getItem("role");
   const handleRegister = async () => {
     try {
       // Validate username
@@ -62,98 +75,135 @@ const RegisterPage = () => {
 
   return (
     <>
-      {role && role.toString() === "student" && <StudentPreProfilePage />}
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Register
-            </h2>
+      {/* {role && role.toString() === "student" && <StudentPreProfilePage />} */}
+      {/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+      <div className=" w-full h-screen flex item-center justify-center 2xl:p-10">
+        <div className="2xl:w-2/5  2xl:rounded-l-2xl xl:w-1/2 lg:w-1/2 md:w-1/2 sm:hidden md:flex xs:hidden flex-col justify-center items-center  bg-gray-100">
+          <div className="registerImage ">
+            <img src={REGISPANDA} alt="" className="h-[30rem] w-[30rem] flex" />
           </div>
-          <form className="mt-8 space-y-6" onSubmit={handleRegister}>
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
+          <div className=" justify-between items-center w-4/5 rounded-xl flex  bottom-10  p-5 ml-10 h-20 shadow-2xl bg-slate-100/50">
+            <span className="text text-gray-700 text-lg font-extrabold">
+              Do you have an account?
+            </span>
+            <Link to={"/register"}>
+              <button
+                type="submit"
+                className="group relative  flex justify-center py-2 px-4 w-[10rem]  text-sm font-medium rounded-2xl text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
               >
-                Username:
-              </label>
-              <input
-                type="text"
-                id="username"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              {usernameError && (
-                <div className="text-red-500">{usernameError}</div>
-              )}
-            </div>
+                Sign in
+              </button>
+            </Link>
+          </div>
+        </div>
 
+        <div className=" 2xl:w-2/5 2xl:rounded-r-2xl   xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-full  xs:w-full flex flex-col justify-center items-center bg-gray-100">
+          <div className="2xl:hidden xl:hidden lg:hidden md:hidden mb-10 sm:mb-3 xs:mb-1 sm:pt-10 flex-row">
+            <img
+              src={LOGO}
+              className="w-[15rem] h-[15rem] xs:w-[10rem] xs:h-[10rem]  flex "
+            />
+          </div>
+          <div className=" flex justify-center items-center flex-row">
+            <img src={EDUME} className="w-40 " />
+          </div>
+          <div className="max-w-md w-full space-y-8">
             <div>
-              <label
-                htmlFor="surname"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Surname:
-              </label>
-              <input
-                type="text"
-                id="surname"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={surname}
-                onChange={(e) => setSurname(e.target.value)}
-              />
-              {surnameError && (
-                <div className="text-red-500">{surnameError}</div>
-              )}
+              <h2 className="mt-5 text-center text-3xl font-extrabold text-gray-900">
+                Register
+              </h2>
             </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email:
-              </label>
-              <input
-                type="text"
-                id="email"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {emailError && <div className="text-red-500">{emailError}</div>}
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password:
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="mt-1 p-2 w-full border rounded-md"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {passwordError && (
-                <div className="text-red-500">{passwordError}</div>
-              )}
-            </div>
-
-            {error && <div className="text-red-500">{error}</div>}
-            <button
-              type="button"
-              onClick={handleRegister}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            <form
+              className="mt-8 space-y-6 w-3/2 p-10 sm:p-1 xs:p-8 xs:ml-3   flex-row"
+              onSubmit={handleRegister}
             >
-              Register
-            </button>
-          </form>
+              <div className="username-wrapper sm:m-0">
+                <label
+                  htmlFor="username"
+                  className="sr-only item-center"
+                ></label>
+                <input
+                  type="text"
+                  id="username"
+                  className="username appearance-none rounded-xl min-h-12 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  value={username}
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                {usernameError && (
+                  <div className="text-red-500">{usernameError}</div>
+                )}
+              </div>
+
+              <div className="surname-wrapper">
+                <label
+                  htmlFor="surname"
+                  className="sr-only item-center"
+                ></label>
+                <input
+                  type="text"
+                  id="surname"
+                  className="username appearance-none rounded-xl min-h-12 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  value={surname}
+                  placeholder="Surname"
+                  onChange={(e) => setSurname(e.target.value)}
+                />
+                {surnameError && (
+                  <div className="text-red-500">{surnameError}</div>
+                )}
+              </div>
+
+              <div className="email-wrapper">
+                <label htmlFor="email" className="sr-only item-center"></label>
+                <input
+                  type="email"
+                  id="email"
+                  className="username appearance-none rounded-xl min-h-12 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  value={email}
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {emailError && <div className="text-red-500">{emailError}</div>}
+              </div>
+
+              <div>
+                <label htmlFor="password" className="sr-only item-center">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="username appearance-none rounded-xl min-h-12 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {passwordError && (
+                  <div className="text-red-500">{passwordError}</div>
+                )}
+              </div>
+
+              {error && <div className="text-red-500">{error}</div>}
+              <button
+                type="button"
+                onClick={handleRegister}
+                className="group  items-center min-h-12 relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-3xl text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              >
+                Register
+              </button>
+            </form>
+          </div>
+
+          <div className="2xl:hidden xl:hidden lg:hidden md:hidden  sm:hidden  justify-center item-center flex-row  w-4/5 rounded-xl flex ">
+            <Link to={"/loginpage"}>
+              <button
+                type="submit"
+                className="group relative w-[10rem] flex justify-center py-2 px-4  text-sm font-medium rounded-2xl text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              >
+                Sign in
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
