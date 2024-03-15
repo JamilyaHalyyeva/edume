1c1a6edc0ab5af1e5b7d98dc557c6da09a34a19d
Jamilya Halyyeva <jamilya.halyyeva@gmail.com>
2024-03-15 13:45:51 +0100

    add: swagger and openapi 3.0.0 support to endpoints

---
+++ server/src/middlewares/swaggerMiddleware.js
@@ -0,0 +1,18 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      version: '1.0.0',
+      description: 'Documentation for Express API',
+    },
+  },
+  apis: ['./src/routes/api/*.js'], // Path to the API routes folder
+};
+
+const specs = swaggerJSDoc(options);
+
+export { specs, swaggerUI };
--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,25 @@
-import express from 'express'
-import config from './config/env.config.js'
-import loggerMiddleware from './middlewares/loggerMiddleware.js'
-import defaultRouter from './routes/api/defaultRoute.js'
-import authRouter from './routes/api/authRoutes.js'
-import connectToDatabase from './config/mongoose.config.js'
-import cors from 'cors'
-const app = express()
+import express from 'express';
+import config from './config/env.config.js';
+import loggerMiddleware from './middlewares/loggerMiddleware.js';
+import defaultRouter from './routes/api/defaultRoute.js';
+import authRouter from './routes/api/authRoutes.js';
+import connectToDatabase from './config/mongoose.config.js';
+import cors from 'cors';
+import { swaggerUI, specs } from './middlewares/swaggerMiddleware.js';
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-connectToDatabase()
-app.use(loggerMiddleware)
-app.use('/api', defaultRouter)
-app.use('/api/auth', authRouter)
+connectToDatabase();
+app.use(loggerMiddleware);
 
-const PORT = config.port || 3000
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+app.use('/api', defaultRouter);
+app.use('/api/auth', authRouter);
+
+const PORT = config.port || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
