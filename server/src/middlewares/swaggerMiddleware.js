c586e312b0e4b83c3401e1c16550be2989497823
Jamilya Halyyeva <jamilya.halyyeva@gmail.com>
2024-03-15 14:12:23 +0100

    edits: swagger description

--- server/src/middlewares/swaggerMiddleware.js
+++ server/src/middlewares/swaggerMiddleware.js
@@ -5,7 +5,7 @@ const options = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Express API with Swagger',
+      title: 'Edume API with Swagger',
       version: '1.0.0',
       description: 'Documentation for Express API',
     },
