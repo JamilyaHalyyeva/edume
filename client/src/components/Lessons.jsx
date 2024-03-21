e87009738738f010c972f36941988deea8c90424
JamilyaHalyyeva <33869456+JamilyaHalyyeva@users.noreply.github.com>
2024-03-22 00:21:27 +0100

    Feature/jamilya/25 register second step (#27)
    * adds: icons for lessons are added
    
    * adds: Student Lessons page in done
    
    * fixs

---
+++ client/src/components/Lessons.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import LessonsCard from "./LessonsCard";
+import { useState } from "react";
+
+// Import lesson images
+import lesson1Image from "../assets/lessons/lesson1.png";
+import lesson2Image from "../assets/lessons/lesson2.png";
+import lesson3Image from "../assets/lessons/lesson3.png";
+import lesson4Image from "../assets/lessons/lesson4.png";
+import lesson5Image from "../assets/lessons/lesson5.png";
+import lesson6Image from "../assets/lessons/lesson6.png";
+import lesson7Image from "../assets/lessons/lesson7.png";
+import lesson8Image from "../assets/lessons/lesson8.png";
+import lesson9Image from "../assets/lessons/lesson9.png";
+import lesson10Image from "../assets/lessons/lesson10.png";
+// Import images for other lessons as needed
+
+const Lessons = () => {
+  const [showAll, setShowAll] = useState(false);
+  // Array containing lesson data
+  const lessons = [
+    {
+      id: 1,
+      description: "Biologie",
+      imageSrc: lesson1Image,
+      bgColor: "bg-green-300",
+    },
+    {
+      id: 2,
+      description: "Mathematik",
+      imageSrc: lesson2Image,
+      bgColor: "bg-red-400",
+    },
+    {
+      id: 3,
+      description: "History",
+      imageSrc: lesson3Image,
+      bgColor: "bg-blue-400",
+    },
+    {
+      id: 4,
+      description: "Music",
+      imageSrc: lesson4Image,
+      bgColor: "bg-pink-400",
+    },
+  ];
+
+  const allLessons = [
+    ...lessons,
+
+    {
+      id: 5,
+      description: "Geography",
+      imageSrc: lesson5Image,
+      bgColor: "bg-yellow-400",
+    },
+    {
+      id: 6,
+      description: "Physics",
+      imageSrc: lesson6Image,
+      bgColor: "bg-green-400",
+    },
+    {
+      id: 7,
+      description: "Biology",
+      imageSrc: lesson7Image,
+      bgColor: "bg-purple-400",
+    },
+    {
+      id: 2,
+      description: "Mathematik",
+      imageSrc: lesson2Image,
+      bgColor: "bg-red-400",
+    },
+
+    {
+      id: 9,
+      description: "English",
+      imageSrc: lesson9Image,
+      bgColor: "bg-cyan-400",
+    },
+    {
+      id: 10,
+      description: "German",
+      imageSrc: lesson10Image,
+      bgColor: "bg-indigo-400",
+    },
+    {
+      id: 3,
+      description: "History",
+      imageSrc: lesson3Image,
+      bgColor: "bg-blue-400",
+    },
+    {
+      id: 8,
+      description: "French",
+      imageSrc: lesson8Image,
+      bgColor: "bg-orange-400",
+    },
+  ];
+
+  // Add more lesson data as needed
+
+  const lessonsToShow = showAll ? allLessons : lessons;
+  return (
+    <div
+      className="  mt-5 justify-center items-center flex-wrap gap-4
+    grid grid-cols-4 mb-[1rem] "
+    >
+      {/* Loop through lessons array and generate LessonCard components */}
+      {lessonsToShow.map((lesson) => (
+        <LessonsCard
+          key={lesson.id}
+          description={lesson.description}
+          imageSrc={lesson.imageSrc}
+          bgColor={lesson.bgColor}
+        />
+      ))}
+      {!showAll && (
+        <div
+          className="col-span-4  mt-6 cursor-pointer  font-serif text-center text-gray-800 "
+          onClick={() => setShowAll(true)}
+        >
+          <button className="rounded-3xl bg-orange-400 w-[8rem] h-[3rem]  shadow-2xl  hover:shadow-orange-600">
+            All Lessons
+          </button>
+        </div>
+      )}
+      {showAll && (
+        <div
+          className="col-span-4 cursor-pointer mt-6 font-serif text-center text-gray-800  "
+          onClick={() => setShowAll(false)}
+        >
+          <button className="  rounded-3xl bg-orange-400 w-[8rem] h-[3rem] shadow-2xl  hover:shadow-orange-600">
+            Less Lessons
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Lessons;
--- client/src/components/StudentDashboard.jsx
+++ client/src/components/StudentDashboard.jsx
@@ -1,5 +1,55 @@
+import Navbar from "./Navbar";
+import StudentNavbar from "./StudentNavbar";
+import Lessons from "./Lessons";
+import FINISHLESSON from "../assets/exer.png";
+import ONLINELESSON from "../assets/online.png";
+import TEST from "../assets/test.png";
+import BGIMAGE from "../assets/bgimage.png";
+import StudentPageFooter from "./StudentPageFooter";
+
 const StudentDashboard = () => {
-  return <div>StudentDashboard</div>;
+  return (
+    <div className="flex justify-center items-center flex-col gap-8">
+      <div className="headerDescription justify-center  items-start font-serif flex bg-orange-400 w-full h-[8rem] flex-col  gap-6">
+        <div className="ml-[15rem] justify-center items-center  ">
+          <h1 className="text-3xl mb-4">Welcome John Doe!! </h1>
+          <h3 className="  text-xl">What do you want to learn today? </h3>
+        </div>
+      </div>
+      <div className=" flex justify-center items-center flex-row gap-10 ">
+        <StudentNavbar
+          img={FINISHLESSON}
+          description={"Exercises  section "}
+          color={"bg-blue-300"}
+        />
+        <StudentNavbar
+          img={ONLINELESSON}
+          description={"Group lessons "}
+          color={"bg-green-300"}
+        />
+        <StudentNavbar
+          img={TEST}
+          description={"Test section "}
+          color={"bg-purple-300"}
+        />
+      </div>
+      <div className="justify-start  items-start  font-serif flex w-full ">
+        <div className="ml-[15rem] justify-start items-center  ">
+          <h1 className="text-2xl mt-4">All Lessons in EduMe!! </h1>
+        </div>
+      </div>
+
+      <Lessons></Lessons>
+
+      <div
+        className=" bg-cover  -m-8  w-10/12 h-[12rem] "
+        style={{ backgroundImage: `url(${BGIMAGE})` }}
+      >
+        {/* Content of your footer */}
+      </div>
+      <StudentPageFooter></StudentPageFooter>
+    </div>
+  );
 };
 
 export default StudentDashboard;
