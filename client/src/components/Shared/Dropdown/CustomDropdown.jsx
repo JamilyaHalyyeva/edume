4d9c44ccefdd53368ca4cec6d1799f6621968bb1
JamilyaHalyyeva <33869456+JamilyaHalyyeva@users.noreply.github.com>
2024-04-04 17:48:44 +0200

    Feature/jamilya/50 teacher preregister page (#52)
    * fixes: styleing and logic for teachers class and grade selection
    
    * fixs:functionalty is editing

---
+++ client/src/components/Shared/Dropdown/CustomDropdown.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+const CustomDropdown = ({ options, onSelect, selectedOption }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleItemClick = (option) => {
+    console.log("handleItemClick->option:", option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative min-w-[200px]" ref={dropdownRef}>
+      {" "}
+      {/* Set a minimum width here */}
+      <button
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className="bg-gray-50 w-full border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 flex justify-between items-center"
+      >
+        {selectedOption ? (
+          <>
+            {/* Ensure you're using the correct image property */}
+            <img
+              src={selectedOption.imageSrc}
+              alt={selectedOption.name}
+              className={`w-6 h-6 mr-2 ${selectedOption.bgColor}`}
+            />
+            {selectedOption.name}
+          </>
+        ) : (
+          "Select an option"
+        )}
+        <FontAwesomeIcon icon={faCaretDown} />
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 w-full bg-white border border-gray-200 mt-1 max-h-60 overflow-auto rounded-lg">
+          <li
+            className="flex items-center p-2  hover:bg-gray-100 cursor-pointer"
+            onClick={() => handleItemClick("")}
+          >
+            Remove Selection
+          </li>
+          {options.map((option) => (
+            <li
+              key={option._id}
+              className="flex items-center p-2 hover:bg-gray-100 cursor-pointer"
+              onClick={() => handleItemClick(option)}
+            >
+              <img
+                src={option.imageSrc} // Ensure this matches the selected option's image property
+                alt={option.name}
+                className={`w-6 h-6 mr-2 ${option.bgColor}`}
+              />
+              {option.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default CustomDropdown;
--- client/src/components/TeacherPreregister/TeacherClassTypeCards.jsx
+++ client/src/components/TeacherPreregister/TeacherClassTypeCards.jsx
@@ -1,94 +1,34 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import config from "../../config/env.config";
-import classTypeImageObjects from "../../assets/classTypes/classTypeImageObjects.js";
+import { useState } from "react";
+
 import TeacherGradeCards from "./TeacherGradeCards.jsx";
 import { useRegister } from "../../context/RegisterProvider.jsx";
-const TeacherClassTypeCards = () => {
-  const { updateUserToBeRegistered, userToBeRegistered } = useRegister();
-  const [classTypeList, setClassTypeList] = useState([]);
-  const [gradeClassTypes, setGradeClassTypes] = useState([]);
-  const [selectedClassType, setSelectedClassType] = useState("");
+import CustomDropdown from "../Shared/Dropdown/CustomDropdown.jsx";
+const TeacherClassTypeCards = ({
+  options,
+  selectedClassType,
+  onSelect,
+  gradeClassTypes,
+}) => {
+  console.log("TeacherClassTypeCards->gradeClassTypes:", gradeClassTypes);
+  const { removeAllGradesWithClassType } = useRegister();
   const [filteredGrades, setFilteredGrades] = useState([]);
-  useEffect(() => {
-    const fetchClassTypes = async () => {
-      try {
-        const response = await axios.get(`${config.apiBaseUrl}/api/classType`, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        });
 
-        const filteredClassTypes = [];
-        response.data.map((ct) => {
-          let match = classTypeImageObjects.find((lesson) => {
-            return lesson.name === ct.name;
-          });
-          if (match) {
-            filteredClassTypes.push({ ...match, _id: ct._id });
-          }
-        });
-
-        console.log("filteredClassTypes:", filteredClassTypes);
-        setClassTypeList(filteredClassTypes);
-        //filterin from previously selected classType from userToBeRegistered
-        if (userToBeRegistered.teacherClassTypeGrades) {
-          const myUnselectedClassTypes = classTypeList.filter(
-            (ctl) =>
-              !userToBeRegistered.teacherClassTypeGrades
-                .map((tctg) => tctg.classType._id)
-                .includes(ctl._id)
-          );
-
-          setClassTypeList(myUnselectedClassTypes);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    const fetchGradeClassTypes = async () => {
-      try {
-        const response = await axios.get(
-          `${config.apiBaseUrl}/api/gradeClassType`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        setGradeClassTypes(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchClassTypes();
-    fetchGradeClassTypes();
-  }, []);
-
-  const handleChange = (event) => {
-    if (event.target.value === "") {
-      console.log(
-        "userToBeRegistered.teacherClassTypesGrades:",
-        userToBeRegistered.teacherClassTypesGrades
-      );
-      updateUserToBeRegistered({
-        teacherClassTypeGrades: [
-          ...userToBeRegistered.teacherClassTypeGrades.filter(
-            (g) => g.classType._id !== selectedClassType._id
-          ),
-        ],
-      });
+  const handleSelect = (selectedOption) => {
+    console.log("selectedOption:", selectedOption);
+    onSelect(selectedOption);
+    if (selectedOption === "") {
+      removeAllGradesWithClassType(selectedClassType);
+    } else if (
+      //check if the selected option is changed from the previous one
+      selectedClassType &&
+      selectedOption._id !== selectedClassType._id
+    ) {
+      removeAllGradesWithClassType(selectedClassType);
     }
-    const mySelectedClassType = classTypeList.find(
-      (ct) => ct._id === event.target.value
-    );
-    setSelectedClassType(mySelectedClassType);
 
-    const myFilteredGrades = filterGrades(event.target.value);
+    //filter the grades based on the selected classType
+
+    const myFilteredGrades = filterGrades(selectedOption._id);
     setFilteredGrades(myFilteredGrades);
   };
 
@@ -105,27 +45,22 @@ const TeacherClassTypeCards = () => {
         return parseInt(a.name, 10) - parseInt(b.name, 10);
       });
   };
+  const selectedOption = () => {
+    console.log("selectedClassType:", selectedClassType);
+    console.log("options:", options);
+    return options.find((option) => option._id === selectedClassType);
+  };
+
   return (
     <div className="w-full ">
       <form className="w-full">
-        <div className="flex justify-start ml-60 items-center">
-          <select
-            id="classTypes"
-            value={
-              selectedClassType && selectedClassType._id
-                ? selectedClassType._id
-                : ""
-            }
-            onChange={handleChange}
-            className="bg-gray-50 w-[10rem] border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-orange-500 block  p-2.5"
-          >
-            <option value="">Choose a lesson</option>
-            {classTypeList.map((classtype) => (
-              <option key={classtype._id} value={classtype._id}>
-                {classtype.name}
-              </option>
-            ))}
-          </select>
+        <div className="flex justify-start ml-20 mb-5 items-center">
+          <CustomDropdown
+            options={options}
+            onSelect={handleSelect}
+            selectedOption={selectedOption()}
+          />
+
           <div className="flex justify-center  items-center">
             <TeacherGradeCards
               grades={filteredGrades}
