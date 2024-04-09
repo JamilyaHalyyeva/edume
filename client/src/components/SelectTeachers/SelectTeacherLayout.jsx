05b5b8327a3e5371159d163117b9fda0c9591417
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-04-10 01:40:43 +0200

    edits: when a classtype selected teacherlist background is having the same color with the icon

--- client/src/components/SelectTeachers/SelectTeacherLayout.jsx
+++ client/src/components/SelectTeachers/SelectTeacherLayout.jsx
@@ -1,8 +1,11 @@
+import { useTeacherSelection } from "../../context/TeacherSelectionProvider";
 import SelectTeacherLesson from "./SelectTeacherLesson";
-import TeacherCard from "./TeacherCard";
+
 import TeacherCardList from "./TeacherCardList";
 
 const SelectTeacherLayout = () => {
+  const { selectedClassType } = useTeacherSelection();
+
   return (
     <div className="flex justify-center items-center flex-col gap-8 w-full md:w-full lg:w-full xl:w-full 2xl:w-full mt-20 ">
       <div className="headerDescription justify-center  items-start font-serif flex bg-orange-400  h-[8rem] w-full flex-col  gap-6">
@@ -12,7 +15,13 @@ const SelectTeacherLayout = () => {
         </div>
       </div>
       <SelectTeacherLesson></SelectTeacherLesson>
-      <TeacherCardList></TeacherCardList>
+      <div
+        className={`w-11/12  shadow-2xl rounded-2xl ${
+          selectedClassType ? selectedClassType.bgColor : "bg-gray-100"
+        } `}
+      >
+        <TeacherCardList></TeacherCardList>
+      </div>
     </div>
   );
 };
