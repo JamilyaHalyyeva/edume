55fdb76f6f200bbc597fc14fef64f3ec740071eb
JamilyaHalyyeva <33869456+JamilyaHalyyeva@users.noreply.github.com>
2024-04-16 15:50:27 +0200

    Styling is still on going (#74)

--- client/src/components/LessonTree/TreeLesson.jsx
+++ client/src/components/LessonTree/TreeLesson.jsx
@@ -15,30 +15,24 @@ const TreeLesson = ({ lesson }) => {
 
   return (
     <div className="lesson">
-      <div className="flex items-center">
-        <button
-          className="flex items-center text-gray-800"
-          onClick={toggleExpand}
-        >
-          <FontAwesomeIcon
-            icon={isExpanded ? faChevronDown : faChevronRight}
-            className="h-5 w-5"
-          />
-        </button>
-        <h3 className="ml-2">{lesson.name}</h3>
-      </div>
+      <div className="flex items-center "></div>
       {isExpanded && (
         <div className="ml-2">
           {lesson.lessonSections && lesson.lessonSections.length > 0 && (
             <div className="lesson-sections">
               {lesson.lessonSections.map((section, index) => (
-                <TreeLessonSection key={index} section={section} />
+                <TreeLessonSection
+                  key={index}
+                  section={section}
+                  isSubSectionEnabled={true}
+                  isChildSection={false}
+                />
               ))}
             </div>
           )}
-          <div className="pipe">|</div>
+
           <button
-            className="btn bg-purple-300 p-2 rounded-md text-black "
+            className="btn bg-orange-400 p-2 rounded-2xl text-black "
             onClick={handleAddSectionClick}
           >
             Add Section
