333c0d9a01843fd3e54f398175e6c4b725de9df4
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-04-24 00:21:31 +0200

    fixes

--- client/src/components/TeacherDashboard.jsx
+++ client/src/components/TeacherDashboard.jsx
@@ -23,18 +23,24 @@ const TeacherDashboard = () => {
         </p>
         <ColumnChartComponent />
       </div>
-      <div className=" p-5 border-2 rounded-2xl  shadow-xl h-[26rem] items-center flex flex-col justify-center">
-        <p className="text-2xl font-bold   text-orange-400 mb-3">
+      {/* <div className=" p-5 border-2 rounded-2xl shadow-xl">
+        <p className="text-2xl font-bold  text-orange-400 mb-3">
           Monthly Attendance
         </p>
         <ColorfulBarChart />
-      </div>
+      </div> */}
       {/* <div className="p-5 border-2">
         <BarChartComponent />
       </div> */}
       {/* <div className="p-5 border-2">
         <LineChartComponent />
       </div> */}
+      <div className="p-5 border-2 rounded-2xl shadow-xl">
+        <p className="text-2xl font-bold text-start text-orange-400 mb-3">
+          Monthly Attendance
+        </p>
+        <ColorfulBarChart />
+      </div>
 
       <div className="p-5 border-2  rounded-2xl shadow-xl">
         <p className="text-2xl font-bold text-start text-orange-400 mb-3">
